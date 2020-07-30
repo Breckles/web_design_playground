d37fcae2f76d0eab8bd1eb3b54dcb4843740f6d9
Waylon Carrier Nunes <wayloncn@gmail.com>
2020-07-30 17:01:35 -0400

    implemented a function to generate circular div elements

--- dist/app.js
+++ dist/app.js
@@ -10,7 +10,16 @@ playButton.addEventListener("click", (event) => {
     if (person) {
         // Placing this here so that invalid field popup still triggers on invalid form submission
         event.preventDefault();
+        let surfaceNavEl = document.querySelector("#surfaceNav");
+        surfaceNavEl.style.display = "flex";
+        surfaceNavEl.append(createOptionNode("test"));
         console.log("Person name present");
     }
 });
+function createOptionNode(content) {
+    let node = document.createElement("div");
+    node.classList.add("node");
+    node.innerText = content;
+    return node;
+}
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,18 @@ playButton.addEventListener("click", (event: Event) => {
   if (person) {
     // Placing this here so that invalid field popup still triggers on invalid form submission
     event.preventDefault();
+    let surfaceNavEl: HTMLDivElement = document.querySelector(
+      "#surfaceNav"
+    ) as HTMLDivElement;
+    surfaceNavEl.style.display = "flex";
+    surfaceNavEl.append(createOptionNode("test"));
     console.log("Person name present");
   }
 });
+
+function createOptionNode(content: string): HTMLDivElement {
+  let node = document.createElement("div");
+  node.classList.add("node");
+  node.innerText = content;
+  return node;
+}
