372648d315a309bebea90c737a818c2e5511fa32
Waylon Carrier Nunes <wayloncn@gmail.com>
2020-08-01 18:39:04 -0400

    nodes are now svg rather than styled html divs. All svg is generated using javascript

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,5 @@
 "use strict";
+const svgNameSpaceURI = "http://www.w3.org/2000/svg";
 let wordTypes = new Map();
 wordTypes.set("adjective", [
     "sorry",
@@ -29,31 +30,51 @@ playButton.addEventListener("click", (event) => {
 });
 // Will be placed in the 'if' statement in the eventlistener above
 let surfaceNavEl = document.querySelector("#surfaceNav");
+let svgCircleEl = document.querySelector("#svgCircle");
 playGame(person, surfaceNavEl);
 ///////////////////////////////////
 function playGame(person, container) {
     container.style.display = "flex";
-    container.append(createOptionNode("test"));
+    container.append(createOptionDial(wordTypes.get("adjective")));
     let blanks = document.getElementsByClassName("blank");
-    console.log(blanks);
+    // console.log(blanks);
     for (const blank of blanks) {
         let wordType = blank.innerText;
-        console.log(wordType);
+        // console.log(wordType);
         if (wordType === "person") {
         }
         else {
             let options = wordTypes.get(wordType);
-            console.log(options);
-            for (const option of options) {
-                container.append(createOptionNode(option));
-            }
+            // console.log(options);
         }
     }
 }
-function createOptionNode(content) {
-    let node = document.createElement("div");
-    node.classList.add("node");
-    node.innerText = content;
+function createOptionDial(optionList) {
+    let x = 20;
+    let y = 20;
+    const svgContainer = document.createElementNS(svgNameSpaceURI, "svg");
+    svgContainer.setAttribute("id", "svgContainer");
+    for (const option of optionList) {
+        let newNode = createOptionNode(option);
+        newNode.setAttribute("transform", `translate(${x}, ${y})`);
+        // newNode.style.transform = `translate(${x}, ${y})`;
+        x += 80;
+        y += 80;
+        svgContainer.append(newNode);
+    }
+    return svgContainer;
+}
+function createOptionNode(textContent) {
+    let node = document.createElementNS(svgNameSpaceURI, "g");
+    node.classList.add("optionNode");
+    node.setAttribute("fill", "white");
+    node.setAttribute("stroke", "black");
+    node.setAttribute("stroke-width", "2");
+    let circle = document.createElementNS(svgNameSpaceURI, "circle");
+    circle.setAttribute("r", "10%");
+    const text = document.createElementNS(svgNameSpaceURI, "text");
+    text.textContent = textContent;
+    node.append(circle, text);
     return node;
 }
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+const svgNameSpaceURI = "http://www.w3.org/2000/svg";
+
 let wordTypes = new Map<string, string[]>();
 wordTypes.set("adjective", [
   "sorry",
@@ -39,35 +41,60 @@ playButton.addEventListener("click", (event: Event) => {
 let surfaceNavEl: HTMLDivElement = document.querySelector(
   "#surfaceNav"
 ) as HTMLDivElement;
+let svgCircleEl: SVGCircleElement = document.querySelector(
+  "#svgCircle"
+) as SVGCircleElement;
+
 playGame(person, surfaceNavEl);
 ///////////////////////////////////
 
 function playGame(person: string, container: HTMLElement) {
   container.style.display = "flex";
-  container.append(createOptionNode("test"));
+  container.append(createOptionDial(wordTypes.get("adjective")!));
   let blanks = document.getElementsByClassName("blank") as HTMLCollectionOf<
     HTMLSpanElement
   >;
-  console.log(blanks);
+  // console.log(blanks);
 
   for (const blank of blanks) {
     let wordType = blank.innerText;
-    console.log(wordType);
+    // console.log(wordType);
     if (wordType === "person") {
     } else {
       let options = wordTypes.get(wordType)!;
-      console.log(options);
-
-      for (const option of options) {
-        container.append(createOptionNode(option));
-      }
+      // console.log(options);
     }
   }
 }
 
-function createOptionNode(content: string): HTMLElement {
-  let node = document.createElement("div");
-  node.classList.add("node");
-  node.innerText = content;
+function createOptionDial(optionList: string[]): SVGSVGElement {
+  let x = 20;
+  let y = 20;
+
+  const svgContainer = document.createElementNS(svgNameSpaceURI, "svg");
+  svgContainer.setAttribute("id", "svgContainer");
+  for (const option of optionList) {
+    let newNode = createOptionNode(option);
+    newNode.setAttribute("transform", `translate(${x}, ${y})`);
+    // newNode.style.transform = `translate(${x}, ${y})`;
+    x += 80;
+    y += 80;
+    svgContainer.append(newNode);
+  }
+  return svgContainer;
+}
+
+function createOptionNode(textContent: string): SVGGElement {
+  let node = document.createElementNS(svgNameSpaceURI, "g");
+  node.classList.add("optionNode");
+  node.setAttribute("fill", "white");
+  node.setAttribute("stroke", "black");
+  node.setAttribute("stroke-width", "2");
+  let circle = document.createElementNS(svgNameSpaceURI, "circle");
+  circle.setAttribute("r", "10%");
+  const text = document.createElementNS(svgNameSpaceURI, "text");
+  text.textContent = textContent;
+  node.append(circle, text);
+
   return node;
 }
